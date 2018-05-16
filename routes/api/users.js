777a0b6a52970aba344549e148713b528a88f603
DamianRivas <drivas1993@gmail.com>
2018-05-16 09:43:07 -0700

    Create login route for users

--- routes/api/users.js
+++ routes/api/users.js
@@ -50,4 +50,30 @@ router.post("/register", (req, res) => {
   });
 });
 
+/**
+ * @route   GET api/users/login
+ * @desc    Login User / Returning JWT Token
+ * @access  Public
+ */
+router.post("/login", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  User.findOne({ email }).then(user => {
+    // Check for User
+    if (!user) {
+      return res.status(404).json({ email: "User not found" });
+    }
+
+    // Check Password
+    bcrypt.compare(password, user.password).then(isMatch => {
+      if (isMatch) {
+        res.json({ msg: "success" });
+      } else {
+        return res.status(400).json({ password: "Incorrect password." });
+      }
+    });
+  });
+});
+
 module.exports = router;
