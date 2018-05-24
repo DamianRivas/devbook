ae70523b028ac9a6e3eddeee49e0f046d6bfeccb
DamianRivas <drivas1993@gmail.com>
2018-05-23 20:05:12 -0700

    Add validation to login form

--- client/src/components/auth/login.js
+++ client/src/components/auth/login.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import axios from "axios";
+import classnames from "classnames";
 
 export default class Login extends Component {
   constructor() {
@@ -15,7 +17,10 @@ export default class Login extends Component {
 
     const { errors, ...user } = this.state;
 
-    console.log(user);
+    axios
+      .post("/api/users/login", user)
+      .then(res => console.log(res.data))
+      .catch(err => this.setState({ errors: err.response.data }));
   };
 
   onChange = e => {
@@ -23,6 +28,9 @@ export default class Login extends Component {
   };
 
   render() {
+    const { errors } = this.state;
+    console.log(errors.email);
+
     return (
       <div className="login">
         <div className="container">
@@ -32,26 +40,36 @@ export default class Login extends Component {
               <p className="lead text-center">
                 Sign in to your DevConnector account
               </p>
-              <form onSubmit={this.onSubmit}>
+              <form noValidate onSubmit={this.onSubmit}>
                 <div className="form-group">
                   <input
                     type="email"
-                    className="form-control form-control-lg"
+                    className={classnames("form-control form-control-lg", {
+                      "is-invalid": errors.email
+                    })}
                     placeholder="Email Address"
                     name="email"
                     value={this.state.email}
                     onChange={this.onChange}
                   />
+                  {errors.email && (
+                    <div className="invalid-feedback">{errors.email}</div>
+                  )}
                 </div>
                 <div className="form-group">
                   <input
                     type="password"
-                    className="form-control form-control-lg"
+                    className={classnames("form-control form-control-lg", {
+                      "is-invalid": errors.password
+                    })}
                     placeholder="Password"
                     name="password"
                     value={this.state.password}
                     onChange={this.onChange}
                   />
+                  {errors.password && (
+                    <div className="invalid-feedback">{errors.password}</div>
+                  )}
                 </div>
                 <input type="submit" className="btn btn-info btn-block mt-4" />
               </form>
